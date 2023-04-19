0897ea38a876a1ecc8eaae772c57d92b9468c8eb
Florian <florianhusson@hotmail.fr>
2023-04-19 18:28:18 +0200

    first impl with template grid

--- CL_ToonBlast/Assets/Scripts/Grid/GridCreator.cs
+++ CL_ToonBlast/Assets/Scripts/Grid/GridCreator.cs
@@ -11,27 +11,41 @@ public class GridCreator : MonoBehaviour
 
     [SerializeField] private GameObject[,] gridCells;
 
-
     [SerializeField] private GameObject cell_PF;
 
+    [SerializeField] private Transform cellsContainer;
+
 #if UNITY_EDITOR
-    [SerializeField] private bool drawDebug; 
+    [SerializeField] private bool drawDebug;
 #endif
 
-    private void Start()
+    private void Reset()
+    {
+        cellsContainer = this.transform;
+    }
+
+    protected virtual void Start()
     {
         CreateGrid();
     }
 
     protected virtual void CreateGrid(bool drawDebug = false)
     {
-        grid = new Grid<GameObject>(gridWidth, gridHeight, cellsSize, this.transform.position - new Vector3(.5f, .5f) * cellsSize,
+        grid = new Grid<GameObject>(gridWidth, gridHeight, cellsSize, cellsContainer.position - new Vector3(.5f, .5f) * cellsSize,
         (x, y) =>
         {
-            GameObject cell = cell_PF.Create(new Vector3(x, y) + this.transform.position);
-            cell.transform.SetParent(this.transform, false);
-            cell.transform.localScale = new Vector3(cellsSize, cellsSize);
-            cell.transform.position *= cellsSize;
+            GameObject cell = cell_PF.Create(new Vector3(x, y) + cellsContainer.position);
+            Transform cellTransform = cell.transform;
+
+            cellTransform.SetParent(cellsContainer, false);
+            cellTransform.localScale = new Vector3(cellsSize, cellsSize);
+            cellTransform.position *= cellsSize;
+
+            GridCell cellScript = cell.GetComponent<GridCell>();
+            cellScript.Setup(x, y);
+
+            if (GameSettings.UnityEditor) cell.name = string.Format($"Cell [ {x}, {y} ]");
+
             return cell;
         }, drawDebug);
     }
